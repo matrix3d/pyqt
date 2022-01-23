206ed1c5d3f93ca37e96ad5dae2786006a206735
matrix3d <sliz@qq.com>
2022-01-23 21:12:55 +0800

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #https://www.youtube.com/watch?v=OZ3MBmuJ5bg&list=PL0uF99K6uCYPp2hv99pcUPpkUtiDWjGdW&index=9
+from PySide2 import QtCore
 from PySide2.QtWidgets import *
 
 
 def onclick():
     print("click")
 
-
+QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 app = QApplication([])
 win = QMainWindow()
 win.resize(800, 600)
