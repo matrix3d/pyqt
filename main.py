0e45dd01211d2ead8021cd7a1d47af2bb12f4708
lizhi <405628079@qq.com>
2022-01-23 13:22:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#https://www.youtube.com/watch?v=OZ3MBmuJ5bg&list=PL0uF99K6uCYPp2hv99pcUPpkUtiDWjGdW&index=9
+from PySide2.QtWidgets import *
+
+
+def onclick():
+    print("click")
+
+
+app = QApplication([])
+win = QMainWindow()
+win.resize(800, 600)
+
+btn = QPushButton("my", win)
+
+btn.clicked.connect(onclick)
+btn.clicked.connect(onclick)
+
+win.show()
+app.exec_()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyqt',
+    version='1',
+    packages=['main'],
+    url='',
+    license='',
+    author='li',
+    author_email='',
+    description=''
+)
