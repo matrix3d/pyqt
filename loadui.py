bcedc7b9aa13da163859915a0d5daf694e47678e
lizhi <405628079@qq.com>
2022-01-25 15:45:44 +0800

    1

--- loadui.py
+++ loadui.py
@@ -1,10 +1,14 @@
 #打包 pyinstaller loadui.py --noconsole --hidden-import PySide2.QtXml
+#https://zhuanlan.zhihu.com/p/390192953
 from PySide2 import QtCore
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import *
 from PySide2.QtWidgets import *
 
 
+def read_qss_file(qss_file_name):
+    with open(qss_file_name, 'r', encoding='UTF-8') as file:
+        return file.read()
 def onclick():
     print("click")
 
@@ -12,6 +16,7 @@ QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 app = QApplication([])
 
 win=QUiLoader().load(QFile("untitled.ui"))
+#app.setStyleSheet(read_qss_file("qdarkstyle/light/style.qss"))
 
 win.show()
 app.exec_()
