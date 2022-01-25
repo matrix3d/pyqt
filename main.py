37e9666524265c0cf7d605bb0a32e9b5cdda36fe
matrix3d <405628079@qq.com>
2022-01-25 21:32:04 +0800

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#https://www.youtube.com/watch?v=OZ3MBmuJ5bg&list=PL0uF99K6uCYPp2hv99pcUPpkUtiDWjGdW&index=9
+# https://www.youtube.com/watch?v=OZ3MBmuJ5bg&list=PL0uF99K6uCYPp2hv99pcUPpkUtiDWjGdW&index=9
 from PySide2 import QtCore
+from PySide2.QtCore import Qt,QUrl
 from PySide2.QtWidgets import *
 
 
 def onclick():
     print("click")
+
 
 QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 app = QApplication([])
@@ -12,6 +14,32 @@ win = QMainWindow()
 win.resize(800, 600)
 
 btn = QPushButton("my", win)
+btn.setAcceptDrops(True)
+
+
+def dragEnterEvent(event):
+    if event.mimeData().hasUrls:
+        event.accept()
+    else:
+        event.ignore()
+
+def dragMoveEvent(event):
+    if event.mimeData().hasUrls:
+        event.setDropAction(Qt.CopyAction)
+        event.accept()
+    else:
+        event.ignore()
+
+def dropEvent(event):
+    if event.mimeData().hasUrls:
+        event.setDropAction(Qt.CopyAction)
+        event.accept()
+        print(event.mimeData().urls())
+
+
+btn.dragEnterEvent = dragEnterEvent
+btn.dragMoveEvent=dragMoveEvent
+btn.dropEvent=dropEvent
 
 btn.clicked.connect(onclick)
 btn.clicked.connect(onclick)
